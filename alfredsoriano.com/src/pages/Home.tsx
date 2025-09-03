e461c37f2cff64da16cbd4f437dc3fe151e0ebc6
alfredsoriano <akcsoriano@gmail.com>
2025-09-03 15:48:50 -0400

    initialize stars

--- alfredsoriano.com/src/pages/Home.tsx
+++ alfredsoriano.com/src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import { ThemeToggle } from "../components/ThemeToggle.tsx";
+import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
             <ThemeToggle />
-        
 
         {/* Background Effect */}
-
+            <StarBackground />
 
         
         {/* Navbar */}
