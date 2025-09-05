a17e5b5a0fd6a9f541a08f75b7d84a361933dc45
alfredsoriano <akcsoriano@gmail.com>
2025-09-05 00:25:03 -0400

    initialize HomeSection component

--- alfredsoriano.com/src/pages/Home.tsx
+++ alfredsoriano.com/src/pages/Home.tsx
@@ -1,18 +1,20 @@
 import { StarBackground } from "../components/StarBackground.tsx";
 import { NavBar } from "../components/NavBar.tsx";
+import { HomeSection } from "../components/HomeSection.tsx";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
         {/* Background Effect */}
-            <StarBackground />
+        <StarBackground />
         
         {/* Navbar */}
-            <NavBar />
+        <NavBar />
 
         {/* Main Content */}
-        
-
+        <main>
+            <HomeSection />
+        </main>
         
         {/* Footer */}
 
