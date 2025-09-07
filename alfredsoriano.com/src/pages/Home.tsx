b50319a1217a4877876982ce47ca347b0a3012cb
alfredsoriano <akcsoriano@gmail.com>
2025-09-07 15:03:25 -0400

    fixes

--- alfredsoriano.com/src/pages/Home.tsx
+++ alfredsoriano.com/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { HomeSection } from "../components/HomeSection.tsx";
 import { AboutSection } from "../components/AboutSection.tsx";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+    return <div className="min-h-screen bg-background text-foreground">
 
         {/* Background Effect */}
         <StarBackground />
